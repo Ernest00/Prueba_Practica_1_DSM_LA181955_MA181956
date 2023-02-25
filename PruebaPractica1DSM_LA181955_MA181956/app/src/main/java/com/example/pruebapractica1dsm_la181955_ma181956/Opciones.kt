7a9f5d8b0fe1237ca8f1ed568e36adf699966bc3
Ernest00 <ernesto.anaya2014@gmail.com>
2023-02-25 17:59:10 -0600

    Enlace a EcuacionCuadratica

--- PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/Opciones.kt
+++ PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/Opciones.kt
@@ -12,6 +12,8 @@ import org.w3c.dom.Text
 
 class Opciones : AppCompatActivity() {
     private lateinit var btn_logout : Button
+    private lateinit var btn_ejercicio1 : Button
+    private lateinit var btn_ejercicio2 : Button
     private lateinit var txt_usuario : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,13 +21,20 @@ class Opciones : AppCompatActivity() {
         setContentView(R.layout.activity_opciones)
         verificarSesion()
 
-        btn_logout = findViewById<Button>(R.id.btn_logout)
         txt_usuario =findViewById<TextView>(R.id.txtUsuario)
+        btn_ejercicio1 = findViewById<Button>(R.id.btn_ejercicio1)
+        btn_ejercicio2 = findViewById<Button>(R.id.btn_ejercicio2)
+        btn_logout = findViewById<Button>(R.id.btn_logout)
 
         val preferences: SharedPreferences = getSharedPreferences("credenciales", MODE_PRIVATE)
 
         val usuarioActivo = preferences.getString("user", "no hay nada cargado")
         txt_usuario.setText(usuarioActivo)
+
+        btn_ejercicio2.setOnClickListener{
+            var intent = Intent( this, EcuacionCuadraticaActivity::class.java)
+            startActivity(intent)
+        }
 
         btn_logout.setOnClickListener {
             restablecerPreferencias()
