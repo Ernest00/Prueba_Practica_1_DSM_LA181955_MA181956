9c7115e38956fc5e9767ebbeff83d1fcb87d122d
Roberto Carlos López Abarca <rcla200031@gmail.com>
2023-02-26 00:07:48 -0600

    Agregando actividad para mostrar resultados de ventas

---
+++ PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/DatosVendedorActivity.kt
@@ -0,0 +1,11 @@
+package com.example.pruebapractica1dsm_la181955_ma181956
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DatosVendedorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_datos_vendedor)
+    }
+}
