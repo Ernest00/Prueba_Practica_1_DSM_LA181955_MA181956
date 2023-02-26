88101b3ecadbf91f0347a8ffcef6dd394c4a4401
Ernest00 <ernesto.anaya2014@gmail.com>
2023-02-26 01:27:29 -0600

    Funcionamiento Ejercicio 2

--- PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/DatosVendedorActivity.kt
+++ PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/DatosVendedorActivity.kt
@@ -1,11 +1,72 @@
 package com.example.pruebapractica1dsm_la181955_ma181956
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class DatosVendedorActivity : AppCompatActivity() {
+    private lateinit var btnVolver : Button
+    private lateinit var txtResNombre : TextView
+    private lateinit var txtResCodigo : TextView
+    private lateinit var txtResVentas : TextView
+    private lateinit var txtResMes : TextView
+    private lateinit var txtResPorcentaje : TextView
+    private lateinit var txtResComisiones : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_datos_vendedor)
+
+        txtResNombre = findViewById<TextView>(R.id.txtResNombre)
+        txtResCodigo = findViewById<TextView>(R.id.txtResCodigo)
+        txtResVentas = findViewById<TextView>(R.id.txtResVentas)
+        txtResMes = findViewById<TextView>(R.id.txtResMes)
+        txtResPorcentaje = findViewById<TextView>(R.id.txtResPorcentaje)
+        txtResComisiones = findViewById<TextView>(R.id.txtResComisiones)
+
+        btnVolver = findViewById<Button>(R.id.btnVolver)
+
+        val bundle = intent.extras
+
+        val nombre = bundle?.getString("nombre").toString()
+        val codigo = bundle?.getString("codigo").toString()
+        val total = bundle?.getString("total").toString()
+        val mes = bundle?.getString("mes").toString()
+        val porc = CalcularPorcentaje(total.toDouble());
+        val comisiones = (total.toDouble() * porc.toDouble())
+
+        txtResNombre.setText(nombre)
+        txtResCodigo.setText(codigo)
+        txtResVentas.setText(total)
+        txtResMes.setText(mes)
+        txtResPorcentaje.setText((porc*100).toString()+" %")
+        txtResComisiones.setText(comisiones.toString())
+
+        btnVolver.setOnClickListener {
+            var intent = Intent(this, FichaVendedorActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+    fun CalcularPorcentaje(total:Double): Double {
+            if(total>=4000) {
+                return 0.3
+            }else if(total>=3000){
+                return 0.2
+            }else if(total>=2000){
+                return 0.15
+            }else if(total>=1000){
+                return 0.1
+            }else if(total>=500){
+                return 0.05
+            }else{
+                return 0.0
+            }
+
     }
 }
--- PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/FichaVendedorActivity.kt
+++ PruebaPractica1DSM_LA181955_MA181956/app/src/main/java/com/example/pruebapractica1dsm_la181955_ma181956/FichaVendedorActivity.kt
@@ -1,5 +1,6 @@
 package com.example.pruebapractica1dsm_la181955_ma181956
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -7,6 +8,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import android.widget.TextView
+import org.w3c.dom.Text
 
 class FichaVendedorActivity : AppCompatActivity() {
     private lateinit var spinner: Spinner
@@ -16,6 +18,10 @@ class FichaVendedorActivity : AppCompatActivity() {
     private lateinit var txtCodigo : EditText
     private lateinit var txtTotalVenta : EditText
 
+    private lateinit var txtErrorNombre : TextView
+    private lateinit var txtErrorCodigo : TextView
+    private lateinit var txtErrorVentas : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ficha_vendedor)
@@ -23,8 +29,11 @@ class FichaVendedorActivity : AppCompatActivity() {
         spinner = findViewById<Spinner>(R.id.listaMeses)
         btnCalcular = findViewById<Button>(R.id.btnCalcularFicha)
         txtNombre = findViewById<EditText>(R.id.txtNombre)
+        txtErrorNombre = findViewById<TextView>(R.id.txtErrorNombre)
         txtCodigo = findViewById<EditText>(R.id.txtCodigo)
+        txtErrorCodigo = findViewById<TextView>(R.id.txtErrorCodigo)
         txtTotalVenta = findViewById<EditText>(R.id.txtTotalVentas)
+        txtErrorVentas = findViewById<TextView>(R.id.txtErrorVentas)
 
         // Cargando el listado de meses desde un array de recursos al objeto spinner (dropdown)
         ArrayAdapter.createFromResource(
@@ -37,7 +46,33 @@ class FichaVendedorActivity : AppCompatActivity() {
         }
 
         btnCalcular.setOnClickListener {
+            if(txtNombre.text.toString().trim().isNotEmpty() && txtCodigo.text.toString().trim().isNotEmpty() && txtTotalVenta.text.toString().trim().isNotEmpty()){
+                var intent = Intent( this, DatosVendedorActivity::class.java)
+                intent.putExtra("nombre", txtNombre.text.toString())
+                intent.putExtra("codigo", txtCodigo.text.toString())
+                intent.putExtra("total", txtTotalVenta.text.toString())
+                intent.putExtra("mes", spinner.selectedItem.toString())
+                startActivity(intent)
+            }
 
+            if(txtNombre.text.toString().trim().isEmpty()){
+txtErrorNombre.setText("No ha ingresado el nombre")
+            }else{
+                txtErrorNombre.setText("")
+            }
+
+            if(txtCodigo.text.toString().trim().isEmpty()){
+                txtErrorCodigo.setText("No ha ingresado el codigo")
+            }else{
+                txtErrorCodigo.setText("")
+            }
+
+            if(txtTotalVenta.text.toString().trim().isEmpty()){
+                txtErrorVentas.setText("No ha ingresado el total de ventas")
+            }else{
+                txtErrorVentas.setText("")
+            }
         }
+
     }
 }
